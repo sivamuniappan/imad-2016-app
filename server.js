374cabfa880ba5e08d621a84a3b3548efd59d80e
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-15 22:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone = {
+    title: 'Article-one i siva muniappan',
+    heading: 'Article-one',
+    date: 'sep 5 2016',
+    content: ` <p>
+                        This is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first article
+                     </p>
+                     <p>
+                        This is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first article
+                     </p>
+                     <p>
+                        This is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first articleThis is the content for my first article
+                     </p>`
+};
+function createtemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+}
+
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+                ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+       
+    </head>
+            <body>
+                <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                   ${heading} 
+                </h3>    
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content};
+                </div>     
+            </body>        
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+  res.send(create template(articleone));
 });
 
 app.get('/article-two', function (req,res) {
