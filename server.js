b01155b066d920000d2ce8a061c63e6f15a1ceaa
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-16 12:34:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,9 +93,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/favicon.ico', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
