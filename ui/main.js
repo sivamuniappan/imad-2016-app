5b41cf4898408f5427e5c697e4c4d858ec143529
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-16 23:10:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,26 +3,12 @@ var button = document.getElementById('counter');
 
 button.onclick = function () {
 
-// Create a request object
-    var request = new XMLHttpRequest();
-
-// capture the response and store in it variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status==200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        // Not yet done
-    };
-
-// Make the request
-   request.open('GET', 'http://sivamuniappan.imad.hasura-app.io/counter' , true );
-   request.send(null);
+request.open('GET','http://sivamuniappan.imad.hasura-app.io/counter',true);
+request.send(null);
 };
+           
+
+
 
 // Submit Name
 var nameInput = document.getElementById('name');
@@ -31,13 +17,29 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and send the name
     
-    // Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
+};// Create a request object
+    var request = new XMLHttpRequest();
+
+// capture the response and store in it variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status==200) {
+             // Capture a list of names and render it as a list
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0; i<names.length; i++) {
     list += '<li>' + names [i] + '</li>'; 
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+        }
+        // Not yet done
+    }
     
 };
+// Make the request
+   request.open('GET', 'http://sivamuniappan.imad.hasura-app.io/submit-name?name='+ name , true );
+   request.send(null);
+}; 
