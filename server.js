3bdcf244fca6395a879f45e17388cb6cec3b5f2a
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-16 22:21:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,13 +102,22 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/favicon.ico:1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico:1'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+var names = [];
+app.get('/submit-name/:name',function(req,res) {
+    // Get the name from the request
+    var name = req.params.name;
+    names.push(name);
+    // JSON : Java Script Object Notation
+    res.send(JSON.stringify(names)); 
+    
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
