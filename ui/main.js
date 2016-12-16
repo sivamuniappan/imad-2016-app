2bc5fc98ef7833bf94f3d06e1ac63ce321c3d734
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-16 13:08:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,14 @@ element.innerHTML = 'new value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function () {
-    img.style.marginleft = '100px';
-    
+   
+  var interval = setInterval(moveright, 100); 
+  
 };
