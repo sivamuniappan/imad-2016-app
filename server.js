c08dbf4ee4faa919a977e913582a0953ff0ee517
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-16 22:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,9 +109,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req,res) {
+app.get('/submit-name',function(req,res) { // URL:/submit-name?name=xxxx
     // Get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     // JSON : Java Script Object Notation
     res.send(JSON.stringify(names)); 
