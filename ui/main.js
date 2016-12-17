642d3db4d410b61fe9fae26692c9ab84451974bd
sivamuniappan <s.sivamuniappan@gmail.com>
2016-12-17 18:50:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,10 @@ button.onclick = function () {
     }
     
 };
-request.open('GET','http://sivamuniappan.imad.hasura-app.io/counter',true);
+request.open('GET', 'http://sivamuniappan.imad.hasura-app.io/counter', true);
 request.send(null);
 };
            
-
-
 
 // Submit Name
 var nameInput = document.getElementById('name');
@@ -31,7 +29,7 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and send the name
     
-};// Create a request object
+   // Create a request object
     var request = new XMLHttpRequest();
 
 // capture the response and store in it variable
@@ -46,15 +44,15 @@ submit.onclick = function () {
     names = JSON.parse(names);
     var list = '';
     for (var i=0; i<names.length; i++) {
-    list += '<li>' + names [i] + '</li>'; 
+    list +- '<li>' + names [i] + '</li>'; 
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
         }
         // Not yet done
+        }
     
-    
-}
+};
 // Make the request
    request.open('GET', 'http://sivamuniappan.imad.hasura-app.io/submit-name?name=' + name , true );
    request.send(null);
